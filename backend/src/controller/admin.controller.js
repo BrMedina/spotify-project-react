5704202f911984652e2ec13fdad4b638b8ded63e
BrMedina <medina.brent26@gmail.com>
2026-06-06 14:10:47 +0800

    feat: implement deleteSong controller and update admin routes for song and album management

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -14,7 +14,7 @@ const uploadToCloudinary = async(file) => {
         throw new Error("Error uploading to cloudinary");
     }
 }
-export const createSong = (req, res, next) => {
+export const createSong = async (req, res, next) => {
     try {
         if(!req.files || !req.files.audioFile || !req.files.imageFile) {
             return res.status(400).json({message: "Please upload all required files."});
@@ -50,4 +50,66 @@ export const createSong = (req, res, next) => {
         console.log("Error in createSong ", error);
         next(error);
     }
+};
+
+export const deleteSong = async (req, res, next) => {
+    try {
+        const {id} = req.params
+
+        const song = await Song.findById(id)
+
+        //if song belongs to an album, update the album's songs array
+        if (song.albumId) {
+            await Album.findByIdAndUpdate(song.albumId, {
+                $pull: {song: song._id},
+            })
+        }
+
+        await Song.findByIdAndDelete(id)
+
+        res.status(200).json({message: "Song deleted successfully"});
+
+        } catch (error){
+            console.log("Error in deleteSong", error);
+            next(error);
+        }
+};
+
+export const createAlbum = async (req, res, next) => {    
+    try {
+        const {title, artist, releaseYear} = req.body
+        const {imageFile} = req.files
+
+        const imageUrl = await uploadToCloudinary(imageFile);
+        
+        const album = new Album ({
+            title,
+            artist,
+            imageUrl,
+            releaseYear
+        })
+
+        await album.save()
+
+        res.status(201).json(album);
+    } catch (error) {
+        console.log("Error in createAlbum", error);
+        next(error);
+        
+    }
+};
+
+export const deleteAlbum = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        await Song.deleteMany({albumId: id});
+        await Album.findByIdAndDelete(id);
+        
+        res.status(200).json({message: "Album deleted successfully."});
+    } catch (error) {
+        console.log("Error in deleteAlbum.", error);
+        next(error);
+        
+    }
+
 };
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createSong } from "../controller/admin.controller.js";
+import { createAlbum, createSong, deleteAlbum, deleteSong } from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middle.js";
 
 const router = Router();
 
 router.post("/songs", protectRoute, requireAdmin, createSong);
+router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+
+router.post("/album", protectRoute, requireAdmin, createAlbum);
+router.post("/album", protectRoute, requireAdmin, deleteAlbum);
 
 
 export default router;
