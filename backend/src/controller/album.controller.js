db112f46c6be286643e6a1ec59181289e6324ba7
BrMedina <medina.brent26@gmail.com>
2026-06-07 02:35:20 +0800

    feat: implement getAllAlbums and getAlbumById controllers in album.controller.js

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -1,2 +1,28 @@
-export const getAllAlbums = (req, res, next) => {};
-export const getAlbumById = (req, res, next) => {};
+import {Album} from "../models/album.model";
+
+export const getAllAlbums = (req, res, next) => {
+    try {
+        const albums = await Album.find();
+        res.status(200).json(albums);
+    } catch (error) {
+        next(error)
+    }
+};
+
+
+export const getAlbumById = async (req, res, next) => {
+    try {
+        const {albumId}= req.params;
+
+        const album = await Album.findById(albumId).populate("songs");
+
+        if (!album) {
+            return res.status(404).json({message: "Album not found."});
+        }
+
+        res.status(200).json(album);
+        
+    } catch (error) {
+        next(error);
+    }
+};
