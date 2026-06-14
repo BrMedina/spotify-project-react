ac8f5e191c13f5bdceb330af56dcbbfffd0982c7
BrMedina <medina.brent26@gmail.com>
2026-06-14 23:06:11 +0800

    feat: enhance LeftSidebar with navigation links and loading skeleton, add ScrollArea component

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -1,10 +1,15 @@
 import { buttonVariants } from '@/components/ui/button'
-import { HomeIcon } from 'lucide-react'
+import { HomeIcon, Library, MessageCircle } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { SignedIn } from '@clerk/clerk-react'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import PlaylistSkeleton from '@/components/skeletons/PlaylistSkeleton'
+
 
 export const LeftSidebar = () => {
+    const isLoading = true;
   return (
     <div className='h-full flex flex-col gap-2'>
         {/* navigation menu*/}
@@ -21,7 +26,40 @@ export const LeftSidebar = () => {
                 <HomeIcon className='mr-2 size-5'/>
                 <span className='hidden md:inline'>Home</span>
                 </Link>
+
+                <SignedIn>
+                    <Link to={"/"}
+
+                    className = {cn(buttonVariants({
+                        variant:'ghost',
+                        className:'w-full justify-start text-white hover:bg-zinc-800',
+                    }))}
+                >
+                <MessageCircle className='mr-2 size-5'/>
+                <span className='hidden md:inline'>Messages</span>
+                </Link> 
+                </SignedIn>
             </div>
+        </div>
+
+        <div className="flex-1 rounded-lg bg-zinc-900 p-4">
+            <div className='flex items-center justify-between mb-4'>
+                <div className="flex items-center text-white px-2">
+                    <Library className='size-5 mr-2'/>
+                    <span className="hidden md:inline">Playlists</span>
+                </div>
+            </div>
+
+            <ScrollArea className='h-[calc(100vh-300px)]'>
+                <div className='space-y-2'>
+                    {isLoading ? (
+                        <PlaylistSkeleton />
+                    ) : (
+                        "some music"
+                    )}
+                </div>
+                
+            </ScrollArea>
         </div>
     </div>
   )
