13bac3e324bb1516946cf3f77104520d147ae73c
BrMedina <medina.brent26@gmail.com>
2026-06-22 22:13:12 +0800

    feat: add FeaturedSection and SectionGrid components for improved layout

---
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -0,0 +1,13 @@
+import { useMusicStore } from '@/stores/useMusicStore'
+import React from 'react'
+
+const FeaturedSection = () => {
+    const {isLoading, featuredSongs, error} =  useMusicStore();
+
+    if(isLoading) return <FeaturedGridSkeleton />
+  return (
+    <div>FeaturedSection</div>
+  )
+}
+
+export default FeaturedSection
