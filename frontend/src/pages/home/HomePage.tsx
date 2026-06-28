333384bc2e6e09685d989d3d89f646a1139a602e
BrMedina <medina.brent26@gmail.com>
2026-06-28 22:44:33 +0800

    feat: restructure HomePage layout with ScrollArea and improved styling

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useMusicStore } from "../../stores/useMusicStore"
 import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 
 const HomePage = () => {
@@ -21,9 +22,21 @@ const HomePage = () => {
   
 
   return (
-    <div className='rounded-md overflow-hidden'>
-      <FeaturedSection/>
-    </div>
+    <main className='rounded-md overflow-hidden h-full bg-linear-to-b from-zinc-800 to-zinc-900'>
+      <ScrollArea className="h-[calc(100vh - 180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
+          <FeaturedSection/>
+        </div>
+
+        <div className="space-y-8">
+          made for you
+          <p>trennding</p>
+        </div>
+      </ScrollArea>
+    </main>
   )
 }
 
