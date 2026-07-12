599d1e1c0b3684b64d071d6e54e07c675a4f6e62
BrMedina <medina.brent26@gmail.com>
2026-07-12 21:19:47 +0800

    feat: implement responsive design in MainLayout and integrate player store in HomePage

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -5,9 +5,20 @@ import PlaybackControls from './components/PlaybackControls.tsx'
 import FriendsActivity from './components/FriendsActivity';
 import TopBar from '@/components/Topbar';
 import AudioPlayer from './components/AudioPlayer';
+import { useEffect, useState } from 'react';
 
 const MainLayout = () => {
-    const isMobile = false;
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkMobile = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        checkMobile();
+        window.addEventListener("resize", checkMobile);
+        return () => window.removeEventListener("resize", checkMobile)
+    })
 
   return (
     <div className='h-screen bg-black text-white flex flex-col'>
@@ -28,12 +39,16 @@ const MainLayout = () => {
                 <Outlet/>
             </ResizablePanel>
 
-            <ResizableHandle className='w-2 bg-black rouned-lg transition-colors'/>
+            {!isMobile && (
+                <>
+                    <ResizableHandle className='w-2 bg-black rouned-lg transition-colors'/>
 
-            {/*right panel */}
-            <ResizablePanel className='min-h-0' defaultSize={24} minSize={0} maxSize={240} collapsedSize={0}>
-                <FriendsActivity />
-            </ResizablePanel>
+                {/*right panel */}
+                    <ResizablePanel className='min-h-0' defaultSize={24} minSize={0} maxSize={240} collapsedSize={0}>
+                        <FriendsActivity />
+                    </ResizablePanel>
+                </>
+            )}
         </ResizablePanelGroup>
 
         <PlaybackControls />
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { useMusicStore } from "../../stores/useMusicStore"
 import FeaturedSection from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SectionGrid from "./components/SectionGrid";
+import { usePlayerStore } from "@/stores/usePlayerStore";
 
 
 const HomePage = () => {
@@ -12,13 +13,24 @@ const HomePage = () => {
     madeForYouSongs,
     featuredSongs,
     trendingSongs,
-   } = useMusicStore();
+  } = useMusicStore();
+
+  const { initializeQueue } = usePlayerStore();
+
+  
 
   useEffect(() => {
     fetchFeaturedSongs();
     fetchMadeForYouSongs();
     fetchTrendingSongs();
   },[fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+
+  useEffect(() => {
+    if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0) {
+      const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
+      initializeQueue(allSongs);
+    }
+  }, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
   
 
