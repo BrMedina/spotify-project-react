4ea9e324c16d3ead1fa5d1fba45717eddac1b2b1
BrMedina <medina.brent26@gmail.com>
2026-07-14 20:29:26 +0800

    feat: add AlbumsTabContent and SongsTabContent components, and update AdminPage with tab functionality

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -1,13 +1,20 @@
 import { useAuthStore } from "@/stores/useAuthStore"
 import { Header } from "./components/Header"
 import { DashboardStats } from "./components/DashboardStats"
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Album, Music } from "lucide-react"
+import { SongsTabContent } from "./components/SongsTabContent"
+import { AlbumsTabContent } from "./components/AlbumsTabContent"
+import { useEffect } from "react"
 
 
 const AdminPage = () => {
     
-    const { isAdmin, isLoading }= useAuthStore()
+    const { isAdmin, isLoading }= useAuthStore();
+
+    useEffect(() => {
+        
+    },[])
 
 
     if (!isAdmin && !isLoading) {
@@ -23,15 +30,22 @@ const AdminPage = () => {
 
         <Tabs defaultValue="songs" className="space-y-6">
             <TabsList className="p-1 bg-zinc-800/50">
-                <TabsTrigger value="songs" className="data-[state = active:] bg-zinc-700">
+                <TabsTrigger value="songs" className="data-[state = active]: bg-zinc-700">
                     <Music className="mr-2 size-4"/>
                     Songs
                 </TabsTrigger>
-                <TabsTrigger value="songs" className="data-[state = active:] bg-zinc-700">
+                <TabsTrigger value="albums" className="data-[state = active]: bg-zinc-700">
                     <Album className="mr-2 size-4"/>
                     Albums
                 </TabsTrigger>
             </TabsList>
+
+            <TabsContent value="songs">
+                <SongsTabContent />
+            </TabsContent>
+            <TabsContent value="albums">
+                <AlbumsTabContent />
+            </TabsContent>
         </Tabs>
 
     </div>
