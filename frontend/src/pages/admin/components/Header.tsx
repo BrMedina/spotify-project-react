275fe052c9839db2183062f91a3e66fd0f7ffc60
BrMedina <medina.brent26@gmail.com>
2026-07-14 20:41:24 +0800

    feat: implement Header component with logo, title, and user button

--- frontend/src/pages/admin/components/Header.tsx
+++ frontend/src/pages/admin/components/Header.tsx
@@ -1,7 +1,27 @@
+import { UserButton } from "@clerk/clerk-react"
+import { Link } from "react-router-dom"
 
 
 export const Header = () => {
   return (
     
-    <div className="flex"></div>
-}
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3 mb-8">
+        <Link to ="/" className="rounded-lg">
+          <img src="/TigerBeats.png" alt="TigerBeats Logo" className="size-16 text-black" />
+        </Link>
+        <div>
+          <h1 className="text-3xl font-bold">
+            Music Manager
+          </h1>
+          <p className="text-zinc-400 mt-1">
+            Manage your Music Catalog.
+          </p>
+        </div>
+      </div>
+      <UserButton/>
+    </div>
+  );
+};
+
+export default Header;
